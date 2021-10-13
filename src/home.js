a11351826ec81b4bbc86749839143afabcab6d9d
CMalatesta <60555786+CMalatesta@users.noreply.github.com>
2021-10-13 10:57:28 -0300

    Update home.js

--- src/home.js
+++ src/home.js
@@ -1,11 +1,9 @@
 const home = (content)=>`
 
     <div class="row mx-auto mt-0 mb-2 mb-lg-3 p-0 container-banner">
-        <h1>
-            <a href="https://ccpexactas.github.io/website/">
-                <img class="p-0" src="${content.attributes.banner}" alt="Construcción Colectiva Profesorxs Exactas (CCPExactas)">
-            </a>
-        </h1>
+        <a href="https://ccpexactas.github.io/website/">
+            <img class="p-0" src="${content.attributes.banner}" alt="Construcción Colectiva Profesorxs Exactas (CCPExactas)">
+        </a>
     </div>
 
     ${content.attributes.title ? `
